6525ca24e26d77e94e4d3853db6815c5e74b579b
jeanfreitas159 <60906231+jeanfreitas159@users.noreply.github.com>
2020-09-01 19:53:17 -0300

    Update jokenpo_java.py

--- jokenpo_java.py
+++ jokenpo_java.py
@@ -1,220 +1,206 @@
 import java.util.Random;
 import java.util.Scanner;
+public class jokenpo_java {
+	
+	public static void menu(){
+		System.out.print("##--Olá Seja Bem vindo ao jogo JoKenpo ninja --##\n\n");        
+		System.out.println("##--Por favor digite seu nome --##\n\n");
 
-public
+		Scanner op1 = new Scanner (System.in);
+	    String palavra = op1.nextLine();
 
+		System.out.print(palavra);
+		System.out.print(" , Digite sua opção de jogo --##\n\n");
+        
+			System.out.print("-----------------------------\n");
+		    System.out.print(" [ 1 ] jogador1   x jogador2 \n");
+			System.out.print(" [ 2 ] computador x jogador \n");
+			System.out.print(" [ 3 ] Opção Sair              \n");
+			System.out.print("-----------------------------\n");	        
+			
+			Scanner entrada = new Scanner(System.in);
 
-class jokenpo_java {
+		}
 
-public static void menu(){
-System.out.print("##--Olá Seja Bem vindo ao jogo JoKenpo ninja --##\n\n");
-System.out.println("##--Por favor digite seu nome --##\n\n");
+		   public static void jogador(){
+		        System.out.println("jogador1 x jogador2.");
+		        
+		        
+		        
+		    }
 
-Scanner op1 = new Scanner (System.in );
-String palavra = op1.nextLine();
+		    public static void pc(){
+		        System.out.println("computador x jogador.");}
+		   
+		        
 
-System.out.print(palavra);
-System.out.print(" , Digite sua opção de jogo --##\n\n");
+		    public static void sair(){
+		        System.out.println("encerrando jogo.");
+		         }
 
-System.out.print("-----------------------------\n");
-System.out.print(" [ 1 ] jogador1   x jogador2 \n");
-System.out.print(" [ 2 ] computador x jogador \n");
-System.out.print(" [ 3 ] Opção Sair              \n");
-System.out.print("-----------------------------\n");
+		  
+	    public static void main(String[] args) {
+	        int opcao;
+	        Scanner entrada = new Scanner(System.in);
 
-Scanner entrada = new Scanner(System.in );
+	        do{
+	            menu();
+	            opcao = entrada.nextInt();
 
-}
+	            switch(opcao){
+	            case 1:
+	                jogador();
+	                
+	                Scanner sc = new Scanner(System.in);
+	   			 Scanner sc1 = new Scanner(System.in);
 
-public static void jogador(){
-System.out.println("jogador1 x jogador2.");
+	   				int jogador1,jogador2;
+	   				
+	   				System.out.println("Escolha sua jogada: ");
+	   				System.out.println("Papel = 0 ");
+	   				System.out.println("Pedra = 1  ");
+	   				System.out.println("Tesoura = 2");
+	   				
+	   				System.out.println("Jogador 1 digite sua jogada : ");
+	   		   		jogador1 = sc.nextInt();
+	   				 
 
-}
+	   			    System.out.println("Jogador 2 digite sua jogada : ");
+	   				jogador2 = sc1.nextInt();
+	   							       		   		
+	   		   		 
+	   		   		 
+	   		   		switch (jogador1) {
+	   		   		 case 0 :
+	   		   		   System.out.println("jogador1 escolheu papel");
+	   		   		    break;
+	   		   		 
+	   		   		 
+	   			     case 1 :
+	   		   		   System.out.println("jogador1 escolheu pedra");
+	   		   		    break;
+	   		   		 
+	   		   		 
+	   			 case 2 :
+	   		   		   System.out.println("jogador1 escolheu tesoura");
+	   		   		    break;
+	   		   		 }
+	   		   		
+	   		   		switch (jogador2){
+	   		   		 case 0 :
+	   		   		   System.out.println("jogador2 escolheu papel");
+	   		   		    break;
+	   		   		 
+	   		   		 
+	   			     case 1 :
+	   		   		   System.out.println("jogador2 escolheu pedra");
+	   		   		    break;
+	   		   		 
+	   		   		 
+	   			 case 2 :
+	   		   		   System.out.println("jogador2 escolheu tesoura");
+	   		   		    break;
+	   		   		 }
+	   		   		 
+	   		   		 
+	   		   	 if ((jogador1 - jogador2)== 0 ) {
+	                    System.out.println("Empate .");
+		   		  }
+	   		   		 
+	   		   		 
+	   		   	 else  if ((jogador1 - jogador2)== - 1 ) {
+	   	                    System.out.println("jogador1 Vencedor .");
+	   		   		  }
+	   		   			 
+	   		   		
+	   		   		  else if ((jogador1 - jogador2)==  2) {
+	   		   			 System.out.println("jogador1 Vencedor .");}
+	   		   			 
+	                       
+	                       else {
+	                       	System.out.println("jogador2 Vencedor .");
+	                       }
+	                   	
+	                break;
 
-public static void pc(){
-System.out.println("computador x jogador.");}
+	            case 2:
+	                pc();
+	                {Scanner leitor = new Scanner (System.in);
 
+		            Random gerador = new Random();
+		   		
+		   		    int usuario ; 
+		   		    int computador;
+		   		    System.out.println("[ 0 ]Papel  ");
+		 		    System.out.println("[ 1 ] Pedra  ");
+		 		    System.out.println("[ 2 ]Tesoura");
+		 		   
+		 		    System.out.println("Digite sua jogada");
+			   		  usuario = leitor.nextInt();
+		   		    
+			   		  computador = gerador.nextInt(3);
+		       
+		   		 
+		   		
+	              switch (computador){
+		   		 case 0 :
+		   		   System.out.println("Computador escolheu papel");
+		   		    break;
+		   		 
+		   		 
+			     case 1:
+		   		   System.out.println("Computador escolheu pedra");
+		   		    break;
+		   		 
+		   		 
+			 case 2 :
+		   		   System.out.println("Computador escolheu tesoura");
+		   		    break;
+		   		 }
+		   		 
+		   		switch (usuario) {
+		   		 case 0 :
+		   		   System.out.println("Usuario escolheu papel");
+		   		    break;
+		   		 
+		   		 
+			     case 1:
+		   		   System.out.println("Usuario escolheu pedra");
+		   		    break;
+		   		 
+		   		 
+			 case 2 :
+		   		   System.out.println("Usuario escolheu tesoura");
+		   		    break;}
+		   	   
+		   		if ((usuario - computador)== 0 ) {
+                    System.out.println("Empate .");
+	   		  }
+   		   		 
+   		   		 
+   		   	 else  if ((usuario - computador)== - 1 ) {
+   	                    System.out.println("Usuario Vencedor .");
+   		   		  }
+   		   			 
+   		   		
+   		   		  else if ((usuario - computador)==  2) {
+   		   			 System.out.println("Usuario Vencedor .");}
+   		   			 
+                       
+                       else {
+                       	System.out.println("Computador Vencedor .");
+                       }
+	                }
+	   		     case 3:
+	                sair();
+	                break;
 
-
-public static void sair(){
-System.out.println("encerrando jogo.");
-}
-
-
-public static void main(String[] args) {
-int opcao;
-Scanner entrada = new Scanner(System.in );
-
-do{
-menu();
-opcao = entrada.nextInt();
-
-switch(opcao){
-case 1:
-    jogador();
-
-    Scanner
-
-
-sc = new
-Scanner(System. in);
-Scanner
-sc1 = new
-Scanner(System. in);
-
-int
-jogador1, jogador2;
-
-System.out.println("Escolha sua jogada: ");
-System.out.println("Papel = 0 ");
-System.out.println("Pedra = 1  ");
-System.out.println("Tesoura = 2");
-
-System.out.println("Jogador 1 digite sua jogada : ");
-jogador1 = sc.nextInt();
-
-System.out.println("Jogador 2 digite sua jogada : ");
-jogador2 = sc1.nextInt();
-
-switch(jogador1)
-{
-    case
-0: \
-    System.out.println("jogador1 escolheu papel");
-break;
-
-case
-1:
-System.out.println("jogador1 escolheu pedra");
-break;
-
-case
-2:
-System.out.println("jogador1 escolheu tesoura");
-break;
-}
-
-switch(jogador2)
-{
-case
-0:
-System.out.println("jogador2 escolheu papel");
-break;
-
-case
-1:
-System.out.println("jogador2 escolheu pedra");
-break;
-
-case
-2:
-System.out.println("jogador2 escolheu tesoura");
-break;
-}
-
-
-if ((jogador1 - jogador2) == 0) {
-System.out.println("Empate .");
-}
-
-else if ((jogador1 - jogador2) == - 1) {
-System.out.println("jogador1 Vencedor .");
-}
-
-else if ((jogador1 - jogador2) == 2) {
-System.out.println("jogador1 Vencedor .");}
-
-else {
-System.out.println("jogador2 Vencedor .");
-}
-
-break;
-
-case
-2:
-pc();
-{Scanner
-leitor = new
-Scanner(System. in);
-
-Random
-gerador = new
-Random();
-
-int
-usuario;
-int
-computador;
-System.out.println("[ 0 ]Papel  ");
-System.out.println("[ 1 ] Pedra  ");
-System.out.println("[ 2 ]Tesoura");
-
-System.out.println("Digite sua jogada");
-usuario = leitor.nextInt();
-
-computador = gerador.nextInt(3);
-
-switch(computador)
-{
-    case
-0: \
-    System.out.println("Computador escolheu papel");
-break;
-
-case
-1:
-System.out.println("Computador escolheu pedra");
-break;
-
-case
-2:
-System.out.println("Computador escolheu tesoura");
-break;
-}
-
-switch(usuario)
-{
-case
-0:
-System.out.println("Usuario escolheu papel");
-break;
-
-case
-1:
-System.out.println("Usuario escolheu pedra");
-break;
-
-case
-2:
-System.out.println("Usuario escolheu tesoura");
-break;}
-
-if ((usuario - computador) == 0) {
-System.out.println("Empate .");
-}
-
-else if ((usuario - computador) == - 1) {
-System.out.println("Usuario Vencedor .");
-}
-
-else if ((usuario - computador) == 2) {
-System.out.println("Usuario Vencedor .");}
-
-else {
-System.out.println("Computador Vencedor .");
-}
-}
-case
-3: \
-    sair();
-break;
-
-default:
-System.out.println("Opção inválida.");
-}
-} while (opcao > 3);
-}
-
-}
+	            default:
+	                System.out.println("Opção inválida.");
+	            }
+	        } while(opcao >3);
+	    }
+	
+	}
 
 
